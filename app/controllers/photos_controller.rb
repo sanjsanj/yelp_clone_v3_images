fffc9196f9122fda06d1328562db0286d7736d3e
Sara OC <saramoo@hotmail.com>
2015-05-08 14:58:02 +0100

    Revert "can add image and display image"
    This reverts commit b28de138a3de739c3af2724827f835cfd586f9f0.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,25 +1,4 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.all
-  end
-  def new
-    @photo = Photo.new
-  end
-  def create
-    Photo.create(photo_params)
-    redirect_to photos_path
-  end
-  def photo_params
-    params.require(:photo).permit(:name, :image)
-  end
-  def show
-    @photo = Photo.find(params[:id])
-  end
-  def edit
-    @photo = Photo.find(params[:id])
-  end
-  def update
-    @photo = Photo.find(params[:id])
-    redirect_to photos_path
   end
 end
